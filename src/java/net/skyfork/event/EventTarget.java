34e86c3cc5883acf974484b85ec764adf2853c48
mc-zhonghuang <mc_zhonghuang@qq.com>
2024-06-16 11:24:30 +0800

    event

--- src/java/net/skyfork/event/EventTarget.java
+++ src/java/net/skyfork/event/EventTarget.java
@@ -1,21 +1,11 @@
 package net.skyfork.event;
 
-import net.skyfork.event.types.Priority;
-
 import java.lang.annotation.*;
 
-/**
- * Marks a method so that the EventManager knows that it should be registered.
- * The priority of the method is also set with this.
- *
- * @author DarkMagician6
- * @see net.skyfork.event.types.Priority
- * @since July 30, 2013
- */
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventTarget {
 
-    byte value() default Priority.MEDIUM;
+    int value() default 10;
 }
